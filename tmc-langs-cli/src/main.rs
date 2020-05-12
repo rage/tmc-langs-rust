fd9f284b8b917c0afa60d4008fb62553e733a6f4
Daniel Martinez <daniel.x.martinez@helsinki.fi>
2020-05-12 18:28:20 +0300

    Outline for CLI

--- tmc-langs-cli/src/main.rs
+++ tmc-langs-cli/src/main.rs
@@ -1,3 +1,169 @@
 //! CLI client for TMC
 
-fn main() {}
+use clap::{App, Arg, SubCommand};
+
+fn main() {
+    let matches = App::new("TestMyCode")
+        .version("0.1.0")
+        .author("Daniel Martinez <daniel.x.martinez@helsinki.fi")
+        .about("CLI client for TMC")
+
+        .subcommand(SubCommand::with_name("checkstyle")
+            .about("Run checkstyle or similar plugin to project if applicable.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("locale")
+                .long("locale")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("compress-project")
+            .about("Compress target project into a ZIP.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("extract-project")
+            .about("Given a downloaded zip, extracts to specified folder.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("prepare-solutions")
+            .about("Prepare a presentable solution from the original.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("prepare-stubs")
+            .about("Prepare a stub exercise from the original.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("prepare-submission")
+            .about("Prepares from submission and solution project for which the tests can be run in sandbox.")
+            .arg(Arg::with_name("clonePath")
+                .long("clonePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("submissionPath")
+                .long("submissionPath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("run-tests")
+            .about("Run the tests for the exercise.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("checkstyleOutputPath")
+                .long("checkstyleOutputPath")
+                .help("Runs checkstyle if defined")
+                .takes_value(true))
+            .arg(Arg::with_name("locale")
+                .long("locale")
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("scan-exercise")
+            .about("Produce an exercise description of an exercise directory.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("find-exercises")
+            .about("Produce list of found exercises.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("get-exercise-packaging-configuration")
+            .about("Returns configuration of under which folders student and nonstudent files are located.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("outputPath")
+                .long("outputPath")
+                .required(true)
+                .takes_value(true)))
+
+        .subcommand(SubCommand::with_name("clean")
+            .about("Clean target directory.")
+            .arg(Arg::with_name("exercisePath")
+                .long("exercisePath")
+                .required(true)
+                .takes_value(true)))
+
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("checkstyle") {
+        println!("checkstyle");
+    } else if let Some(matches) = matches.subcommand_matches("compress-project") {
+        println!("compress-project");
+    } else if let Some(matches) = matches.subcommand_matches("extract-project") {
+        println!("extract-project");
+    } else if let Some(matches) = matches.subcommand_matches("prepare-solutions") {
+        println!("prepare-solutions");
+    } else if let Some(matches) = matches.subcommand_matches("prepare-stubs") {
+        println!("prepare-stubs");
+    } else if let Some(matches) = matches.subcommand_matches("prepare-submission") {
+        println!("prepare-submission");
+    } else if let Some(matches) = matches.subcommand_matches("run-tests") {
+        println!("run-tests");
+    } else if let Some(matches) = matches.subcommand_matches("scan-exercise") {
+        println!("scan-exercise");
+    } else if let Some(matches) = matches.subcommand_matches("find-exercises") {
+        println!("find-exercises");
+    } else if let Some(matches) = matches.subcommand_matches("get-exercise-packaging-configuration")
+    {
+        println!("get-exercise-packaging-configuration");
+    } else if let Some(matches) = matches.subcommand_matches("clean") {
+        println!("clean");
+    }
+}
