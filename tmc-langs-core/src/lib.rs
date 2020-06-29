0d53bee85524c8fd275ad0b5cd3fececa968cdef
Daniel Martinez <daniel.x.martinez@helsinki.fi>
2020-06-29 14:38:32 +0300

    add core exports

--- tmc-langs-core/src/lib.rs
+++ tmc-langs-core/src/lib.rs
@@ -9,8 +9,8 @@ pub use error::CoreError;
 pub use request::FeedbackAnswer;
 pub use response::{
     Course, CourseData, CourseDataExercise, CourseDataExercisePoint, CourseDetails, CourseExercise,
-    ExerciseDetails, NewSubmission, Organization, Review, Submission, SubmissionFeedbackResponse,
-    User,
+    Exercise, ExerciseDetails, NewSubmission, Organization, Review, Submission,
+    SubmissionFeedbackResponse, SubmissionFinished, SubmissionProcessingStatus, User,
 };
 pub use tmc_core::TmcCore;
 pub use tmc_langs_util::{Language, RunResult, ValidationResult};
