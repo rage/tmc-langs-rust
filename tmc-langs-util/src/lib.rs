d29e757915bed433adc728ef606646d3a0c6a1f0
Daniel Martinez <daniel.x.martinez@helsinki.fi>
2020-06-29 17:24:26 +0300

    add export for RunStatus

--- tmc-langs-util/src/lib.rs
+++ tmc-langs-util/src/lib.rs
@@ -5,7 +5,7 @@ pub mod task_executor;
 
 pub use tmc_langs_abstraction::ValidationResult;
 pub use tmc_langs_framework::{
-    domain::{ExerciseDesc, ExercisePackagingConfiguration, RunResult},
+    domain::{ExerciseDesc, ExercisePackagingConfiguration, RunResult, RunStatus},
     plugin::Language,
     Error,
 };
