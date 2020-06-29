58cff3edf77075f8400fb3516025695e230cc3f9
Daniel Martinez <daniel.x.martinez@helsinki.fi>
2020-06-29 18:06:05 +0300

    added java test

--- tmc-langs-core/tests/integration.rs
+++ tmc-langs-core/tests/integration.rs
@@ -9,7 +9,7 @@ use std::time::Duration;
 use tmc_langs_core::{
     CoreError, Exercise, SubmissionFinished, SubmissionProcessingStatus, TmcCore,
 };
-use tmc_langs_util::Language;
+use tmc_langs_util::{Language, RunStatus};
 use url::Url;
 
 const TMC_ROOT: &str = "https://tmc.mooc.fi/";
@@ -30,45 +30,103 @@ fn authenticated_core() -> TmcCore {
     core
 }
 
+// downloads and submits all exercises for course, asserts that the tests are run but fail
+fn dl_test_submit_course_templates(course_id: usize) {
+    init();
+
+    fn submitter(core: &TmcCore, exercise: Exercise) -> SubmissionFinished {
+        let id = exercise.id;
+        dl_test_submit_exercise(
+            &core,
+            exercise,
+            |target| core.download_or_update_exercises(vec![(id, target)]),
+            false,
+        )
+    }
+
+    dl_test_submit_course_exercises(course_id, submitter, false);
+}
+
+// downloads and submits all exercise solutions for course, asserts that tests pass
+fn dl_test_submit_course_solutions(course_id: usize) {
+    init();
+
+    fn submitter(core: &TmcCore, exercise: Exercise) -> SubmissionFinished {
+        let solution_url = Url::parse(&exercise.return_url)
+            .unwrap()
+            .join("solution/download")
+            .unwrap();
+        dl_test_submit_exercise(
+            &core,
+            exercise,
+            |target| core.download_model_solution(solution_url, target),
+            true,
+        )
+    }
+
+    dl_test_submit_course_exercises(course_id, submitter, true);
+}
+
 // fetches course exercises and runs submitter on each one
-// submitter should submit the exercise
-// assert should assert something about each submission result
-fn submit_downloaded_course_exercises<F, A>(course_id: usize, submitter: F, assert: A)
+// tester_submitter should test and submit the exercise
+fn dl_test_submit_course_exercises<F>(course_id: usize, tester_submitter: F, should_pass: bool)
 where
     F: Fn(&TmcCore, Exercise) -> SubmissionFinished,
-    A: Fn(SubmissionFinished) -> (),
 {
     log::debug!("fetching course {}", course_id);
     let core = authenticated_core();
     let course_details = core.get_course_details(course_id).unwrap();
-    log::debug!("submitting course templates for {:#?}", course_details);
+    log::debug!(
+        "testing and submitting course templates for {:#?}",
+        course_details
+    );
 
     for exercise in course_details.exercises {
+        if exercise.name.contains("osa01")
+            || exercise.name.contains("osa02")
+            || exercise.name.contains("osa03")
+            || exercise.name.contains("osa04")
+            || exercise.name.contains("osa05")
+        {
+            // temp, these parts have been checked already for java
+            continue;
+        }
         if [93659, 92964, 92960, 82587].contains(&exercise.id) {
             log::info!("skipping {}: solution does not pass tests", exercise.id);
             continue;
         }
-        if exercise.name.contains("osa1") {
-            // temp
+        if [94570, 94726].contains(&exercise.id) {
+            log::info!("skipping {}: template always passes", exercise.id);
             continue;
         }
-        let finished = submitter(&core, exercise);
-        assert(finished)
+
+        let finished = tester_submitter(&core, exercise);
+        log::debug!("finished {:#?}", finished);
+        assert_eq!(should_pass, finished.all_tests_passed.unwrap());
     }
 }
 
 // submits the exercise
 // downloader should download the submission target to the path arg
-fn submit_downloaded_exercise<F: FnOnce(&Path) -> Result<(), CoreError>>(
+fn dl_test_submit_exercise<F: FnOnce(&Path) -> Result<(), CoreError>>(
     core: &TmcCore,
     exercise: Exercise,
     downloader: F,
+    should_pass: bool,
 ) -> SubmissionFinished {
     log::debug!("submitting exercise {:#?}", exercise);
     let temp = tempfile::tempdir().unwrap();
     let submission_path = temp.path().join(exercise.id.to_string());
     log::debug!("downloading to {}", submission_path.display());
     downloader(&submission_path).unwrap();
+
+    log::debug!("testing locally {}", submission_path.display());
+    let test_results = core.run_tests(&submission_path).unwrap();
+    if should_pass {
+        assert_eq!(test_results.status, RunStatus::Passed);
+    } else {
+        assert_eq!(test_results.status, RunStatus::TestsFailed);
+    }
 
     let submission_url = Url::parse(&exercise.return_url).unwrap();
     log::debug!("submitting to {}", submission_url);
@@ -97,38 +155,50 @@ mod python {
     #[test]
     #[ignore]
     // passed 29.6.2020
-    fn submit_python_course_templates() {
-        init();
-
-        fn submitter(core: &TmcCore, exercise: Exercise) -> SubmissionFinished {
-            let id = exercise.id;
-            submit_downloaded_exercise(&core, exercise, |target| {
-                core.download_or_update_exercises(vec![(id, target)])
-            })
-        }
-
-        submit_downloaded_course_exercises(PYTHON_COURSE_ID, submitter, |finished| {
-            assert!(!finished.all_tests_passed.unwrap())
-        });
+    fn templates() {
+        dl_test_submit_course_templates(PYTHON_COURSE_ID)
     }
 
     #[test]
     #[ignore]
-    fn submit_python_course_solutions() {
-        init();
+    // passed 29.6.2020
+    fn solutions() {
+        dl_test_submit_course_solutions(PYTHON_COURSE_ID)
+    }
+}
 
-        fn submitter(core: &TmcCore, exercise: Exercise) -> SubmissionFinished {
-            let solution_url = Url::parse(&exercise.return_url)
-                .unwrap()
-                .join("solution/download")
-                .unwrap();
-            submit_downloaded_exercise(&core, exercise, |target| {
-                core.download_model_solution(solution_url, target)
-            })
-        }
+mod java {
+    use super::*;
 
-        submit_downloaded_course_exercises(PYTHON_COURSE_ID, submitter, |finished| {
-            assert!(finished.all_tests_passed.unwrap())
-        });
+    const JAVA_COURSE_ID: usize = 665;
+
+    #[test]
+    #[ignore]
+    fn templates() {
+        dl_test_submit_course_templates(JAVA_COURSE_ID)
+    }
+
+    #[test]
+    #[ignore]
+    fn solutions() {
+        dl_test_submit_course_solutions(JAVA_COURSE_ID)
+    }
+}
+
+mod csharp {
+    use super::*;
+
+    const CSHARP_COURSE_ID: usize = 651;
+
+    #[test]
+    #[ignore]
+    fn templates() {
+        dl_test_submit_course_templates(CSHARP_COURSE_ID)
+    }
+
+    #[test]
+    #[ignore]
+    fn solutions() {
+        dl_test_submit_course_solutions(CSHARP_COURSE_ID)
     }
 }
