bbba20334fbc3f64ef255140ad507c5f7c89c42d
Heliozoa <Heliozoa@users.noreply.github.com>
2020-07-17 05:46:34 +0000

    Deploying to gh-pages from  @ 707d6e7cbe891f6b95bf4d22e69f0c10ea3655e4 🚀

--- source-files.js
+++ source-files.js
@@ -1,7 +1,7 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["adler"] = {"name":"","files":["algo.rs","lib.rs"]};
 sourcesIndex["aho_corasick"] = {"name":"","dirs":[{"name":"packed","dirs":[{"name":"teddy","files":["compile.rs","mod.rs","runtime.rs"]}],"files":["api.rs","mod.rs","pattern.rs","rabinkarp.rs","vector.rs"]}],"files":["ahocorasick.rs","automaton.rs","buffer.rs","byte_frequencies.rs","classes.rs","dfa.rs","error.rs","lib.rs","nfa.rs","prefilter.rs","state_id.rs"]};
-sourcesIndex["ansi_term"] = {"name":"","files":["ansi.rs","debug.rs","difference.rs","display.rs","lib.rs","style.rs","util.rs","windows.rs","write.rs"]};
+sourcesIndex["ansi_term"] = {"name":"","files":["ansi.rs","debug.rs","difference.rs","display.rs","lib.rs","style.rs","windows.rs","write.rs"]};
 sourcesIndex["anyhow"] = {"name":"","files":["backtrace.rs","chain.rs","context.rs","error.rs","fmt.rs","kind.rs","lib.rs","macros.rs","wrapper.rs"]};
 sourcesIndex["atty"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["base64"] = {"name":"","dirs":[{"name":"read","files":["decoder.rs","mod.rs"]},{"name":"write","files":["encoder.rs","mod.rs"]}],"files":["chunked_encoder.rs","decode.rs","display.rs","encode.rs","lib.rs","tables.rs"]};
--- tmc_langs_cli/sidebar-items.js
+++ tmc_langs_cli/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["find_exercise_directories",""],["into_locale",""],["into_url",""],["into_usize",""],["main",""],["print_result_as_json",""],["run",""],["run_checkstyle",""],["write_result_to_file_as_json",""]]});
+initSidebarItems({"fn":[["create_app",""],["find_exercise_directories",""],["into_locale",""],["into_url",""],["into_usize",""],["main",""],["print_result_as_json",""],["run",""],["run_checkstyle",""],["write_result_to_file_as_json",""]]});
