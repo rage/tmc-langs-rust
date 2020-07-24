ced210f3839e9a7612d88cde90434bbed2b4209e
Heliozoa <Heliozoa@users.noreply.github.com>
2020-07-24 08:19:15 +0000

    Deploying to gh-pages from  @ 082148e6fc3b5bb8ad12f0791f28d033db7ec3f4 🚀

---
+++ tmc_langs_cli/output/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["OutputResult",""],["Status",""]],"struct":[["Output",""]]});
--- tmc_langs_cli/sidebar-items.js
+++ tmc_langs_cli/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["create_app",""],["find_exercise_directories",""],["into_locale",""],["into_url",""],["into_usize",""],["main",""],["print_result_as_json",""],["run",""],["run_checkstyle",""],["write_result_to_file_as_json",""]]});
+initSidebarItems({"fn":[["find_exercise_directories",""],["into_locale",""],["into_url",""],["into_usize",""],["main",""],["print_output",""],["run",""],["run_checkstyle",""],["write_result_to_file_as_json",""]],"mod":[["app","Create clap app"],["output","Output format"]]});
