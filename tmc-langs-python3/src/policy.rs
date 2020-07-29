5f164187cbf82dcf3dc664673dbac034466db6a9
Daniel Martinez <daniel.x.martinez@helsinki.fi>
2020-07-29 09:33:49 +0300

    set __pycache__ and .pyc files to not be student source files

--- tmc-langs-python3/src/policy.rs
+++ tmc-langs-python3/src/policy.rs
@@ -1,5 +1,6 @@
 //! Contains the Python3 student file policy
 
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use tmc_langs_framework::policy::StudentFilePolicy;
 
@@ -22,5 +23,35 @@ impl StudentFilePolicy for Python3StudentFilePolicy {
 
     fn is_student_source_file(&self, path: &Path) -> bool {
         path.starts_with("src")
+            && path.extension() != Some(OsStr::new("pyc"))
+            && !path
+                .components()
+                .any(|c| c.as_os_str() == OsStr::new("__pycache__"))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn in_src_is_source_file() {
+        let policy = Python3StudentFilePolicy::new(PathBuf::from(""));
+        assert!(policy.is_student_source_file(Path::new("src/some_file.py")));
+    }
+
+    #[test]
+    fn pycache_is_not_source_file() {
+        let policy = Python3StudentFilePolicy::new(PathBuf::from(""));
+        assert!(!policy.is_student_source_file(Path::new("__pycache__")));
+        assert!(!policy.is_student_source_file(Path::new("__pycache__/cachefile")));
+        assert!(!policy.is_student_source_file(Path::new("src/__pycache__")));
+    }
+
+    #[test]
+    fn pyc_is_not_source_file() {
+        let policy = Python3StudentFilePolicy::new(PathBuf::from(""));
+        assert!(!policy.is_student_source_file(Path::new("some.pyc")));
+        assert!(!policy.is_student_source_file(Path::new("src/other.pyc")));
     }
 }
