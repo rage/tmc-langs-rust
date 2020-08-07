d4f5ef9c82b03c45533191b920488ecef7939f09
Heliozoa <Heliozoa@users.noreply.github.com>
2020-08-07 13:30:25 +0000

    Deploying to gh-pages from  @ fb0ae0c780ad066aa5a50a0543bfd3df577e1f49 🚀

--- tmc_langs_cli/app/sidebar-items.js
+++ tmc_langs_cli/app/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["SCHEMA_NULL",""],["SCHEMA_TOKEN",""]],"fn":[["create_app",""],["create_core_app",""],["schema_leaked",""]]});
+initSidebarItems({"constant":[["SCHEMA_NULL",""],["SCHEMA_TOKEN",""]],"fn":[["create_app","Constructs the CLI root."],["create_core_app","Constructs the core sub-command."],["schema_leaked",""]]});
