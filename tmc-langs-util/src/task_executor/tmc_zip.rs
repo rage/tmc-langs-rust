cd6f7444b750f154c2ac0be801375c89839dc0c5
Daniel Martinez <daniel.x.martinez@helsinki.fi>
2020-12-08 10:05:58 +0200

    fix windows test

--- tmc-langs-util/src/task_executor/tmc_zip.rs
+++ tmc-langs-util/src/task_executor/tmc_zip.rs
@@ -333,7 +333,7 @@ mod test {
     fn windows_paths_get_converted() {
         let win_path = PathBuf::from(r"tests\data\dir");
         let zipped = zip(
-            EverythingIsStudentFilePolicy::new(win_path.clone()),
+            EverythingIsStudentFilePolicy::new(win_path.clone()).unwrap(),
             &win_path,
         )
         .unwrap();
