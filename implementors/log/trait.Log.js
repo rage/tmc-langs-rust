a24a2ce67debcb99e05b954fdad83ecb0026845a
Heliozoa <Heliozoa@users.noreply.github.com>
2021-01-27 15:30:39 +0000

    Deploying to gh-pages from  @ 466156b92ffb3d31c81e001d14295ea650eaf471 🚀

--- implementors/log/trait.Log.js
+++ implementors/log/trait.Log.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["env_logger"] = [{"text":"impl Log for Logger","synthetic":false,"types":[]}];
+implementors["log"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
