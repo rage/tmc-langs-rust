c60934bd93589df7ebe084da0765f2eaef933924
Daniel Martinez <daniel.x.martinez@helsinki.fi>
2021-01-28 08:10:36 +0200

    added test data

---
+++ plugins/csharp/tests/data/partially-passing/src/TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestProject
+{
+    public class Program
+    {
+        public static bool ReturnTrue => true;
+
+        public static bool ReturnNotInput(bool input) => !input;
+        public static string ReturnInputString(string input) => input;
+
+        public static void Main(string[] args)
+        {
+			//BEGIN SOLUTION
+            Console.WriteLine("Hello Home");
+			//END SOLUTION
+			//STUB: Console.WriteLine("Stub");
+        }
+    }
+}
