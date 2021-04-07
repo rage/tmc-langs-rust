4339789b8a373262a1bf8c096f8ad6c8a543b91c
Heliozoa <Heliozoa@users.noreply.github.com>
2021-04-07 11:24:51 +0000

    Deploying to gh-pages from  @ 91a02c6efe1abf920a7e5cbb8f1567f013f5763a 🚀

--- implementors/serde/ser/trait.Error.js
+++ implementors/serde/ser/trait.Error.js
@@ -1,8 +1,8 @@
 (function() {var implementors = {};
 implementors["serde"] = [];
-implementors["serde_json"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["serde_urlencoded"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["serde_xml_rs"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["serde_yaml"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
-implementors["toml"] = [{"text":"impl Error for Error","synthetic":false,"types":[]}];
+implementors["serde_json"] = [{"text":"impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"struct\" href=\"serde_json/struct.Error.html\" title=\"struct serde_json::Error\">Error</a>","synthetic":false,"types":["serde_json::error::Error"]}];
+implementors["serde_urlencoded"] = [{"text":"impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"serde_urlencoded/ser/enum.Error.html\" title=\"enum serde_urlencoded::ser::Error\">Error</a>","synthetic":false,"types":["serde_urlencoded::ser::Error"]}];
+implementors["serde_xml_rs"] = [{"text":"impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"serde_xml_rs/enum.Error.html\" title=\"enum serde_xml_rs::Error\">Error</a>","synthetic":false,"types":["serde_xml_rs::error::Error"]}];
+implementors["serde_yaml"] = [{"text":"impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"struct\" href=\"serde_yaml/struct.Error.html\" title=\"struct serde_yaml::Error\">Error</a>","synthetic":false,"types":["serde_yaml::error::Error"]}];
+implementors["toml"] = [{"text":"impl <a class=\"trait\" href=\"serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"toml/ser/enum.Error.html\" title=\"enum toml::ser::Error\">Error</a>","synthetic":false,"types":["toml::ser::Error"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- tmc_langs/warning_reporter/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["init","Initializes the warning reporter with the given closure to be called with any warnings. Can only be initialized once, repeated calls do nothing."],["warn","Calls the warning closure with the given warning."]],"struct":[["Warning",""]]});
