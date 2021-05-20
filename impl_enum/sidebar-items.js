233b35017bba80a72947e75e276adeeff87646c3
Heliozoa <Heliozoa@users.noreply.github.com>
2021-05-20 07:22:58 +0000

    Deploying to gh-pages from  @ 12f021ba4c823c9831ec6e63652fb7a11c67b69a 🚀

--- impl_enum/sidebar-items.js
+++ impl_enum/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"attr":[["with_methods","Generates an impl block for an enum containing the given methods, where the method is a simple match over all the variants, calling the same method on the matched variant's first field."]]});
+initSidebarItems({"attr":[["with_methods","Generates an impl block for an enum containing the given methods, where the method is a simple match over all the variants, calling the same method on the matched variant’s first field."]]});
