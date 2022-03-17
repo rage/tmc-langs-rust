5fcf8c91cda20e64d5e0432d76867d6a4cdcf08a
Heliozoa <Heliozoa@users.noreply.github.com>
2022-03-17 18:03:00 +0000

    Deploying to gh-pages from  @ d141c80d760253931840ea8f02a337203b918c98 🚀

---
+++ tmc_langs/progress_reporter/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["finish_stage","Finishes the current stage."],["progress_stage","Progresses the current stage."],["start_stage","Starts a new stage."],["subscribe","Subscribes to progress reports of type T with callback of type F called every time progress is made with type T."]],"struct":[["StatusUpdate","The format for all status updates. May contain some data."]]});
