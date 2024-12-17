5fe366d9cf81bd56476f8c69081cec30f8a3840b
Heliozoa <Heliozoa@users.noreply.github.com>
2024-12-17 05:12:06 +0000

    Deploying to gh-pages from @ rage/tmc-langs-rust@296031b832faec9a46d59b1e00e04988a8f8564a 🚀

--- trait.impl/alloc/borrow/trait.ToOwned.js
+++ trait.impl/alloc/borrow/trait.ToOwned.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["winnow",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"winnow/stream/struct.BStr.html\" title=\"struct winnow::stream::BStr\">BStr</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"winnow/stream/struct.Bytes.html\" title=\"struct winnow::stream::Bytes\">Bytes</a>"]]]]);
+    var implementors = Object.fromEntries([["winnow",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"winnow/stream/struct.BStr.html\" title=\"struct winnow::stream::BStr\">BStr</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"winnow/stream/struct.Bytes.html\" title=\"struct winnow::stream::Bytes\">Bytes</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
